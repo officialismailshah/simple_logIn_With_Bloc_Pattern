246c45b78109230009217d3e252e2eb58346e811
ismail <smily124000@gmail.com>
2021-05-02 12:53:03 +0500

    remove the spare dependencies

--- lib/blocs/provider.dart
+++ lib/blocs/provider.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'bloc.dart';
 
@@ -7,7 +5,7 @@ class Provider extends InheritedWidget {
   bool updateShouldNotify(_) => true;
 // this is an instance of Provider Class
   final bloc = new Bloc();
-  Provider(Key key, Widget child) : super(key: key, child: child);
+  Provider({Key key, Widget child}) : super(key: key, child: child);
 // Static function which will be called automatically
   static Bloc of(BuildContext context) {
     return (context.dependOnInheritedWidgetOfExactType() as Provider).bloc;
